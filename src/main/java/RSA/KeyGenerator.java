4283321cc2adbe16cff2653d471e402addbffadf
JoaoPedroCNMota <joaopedrocnmota@gmail.com>
2022-09-18 11:55:52 -0300

    remove teste de esrastotenes

--- src/main/java/RSA/KeyGenerator.java
+++ src/main/java/RSA/KeyGenerator.java
@@ -16,47 +16,53 @@ public class KeyGenerator {
     //https://acervolima.com/como-gerar-grandes-numeros-primos-para-algoritmo-rsa/
     //https://www.geeksforgeeks.org/how-to-generate-large-prime-numbers-for-rsa-algorithm/
 
-    public BigInteger generateRandomPrimeNumber(){
+    public BigInteger getRandomPrimeNumber(){
         BigInteger probablePrime = BigInteger.probablePrime(MIN_BIT_LEN, random);
+        boolean isPrime = millerRabin(probablePrime.intValue());
 
-        lowLevelPrimeTest(probablePrime.intValue());
-        millerRabin(probablePrime.intValue());
+        if (isPrime)
+            System.out.println("Teste");
+        while (!isPrime){
+            probablePrime = BigInteger.probablePrime(MIN_BIT_LEN, random);
+            isPrime = millerRabin(probablePrime.intValue());
+        }
+
         return probablePrime;
     }
 
     /*Teste de primalidade de baixo nível,
     * verifica se provavel primo eh divisivel por seus N primeiros primos antecessores*/
-    private boolean lowLevelPrimeTest(int primeCandidate){
-        List<Integer> firstPrimes = eratosthenesSieve(primeCandidate);
-        for (Integer prime: firstPrimes) {
-            if (primeCandidate % prime == 0 && Math.pow(prime, 2) <= primeCandidate)
-                return false;
-        }
-        return true;
-    }
+//    private boolean lowLevelPrimeTest(BigInteger primeCandidate){
+//        List<Integer> firstPrimes = eratosthenesSieve(primeCandidate);
+//        for (Integer prime: firstPrimes) {
+//            if (primeCandidate % prime == 0 && Math.pow(prime, 2) <= primeCandidate)
+//                return false;
+//        }
+//        return true;
+//    }
 
-    private List<Integer> eratosthenesSieve(int num){
-        boolean[] primes = new boolean[num + 1];
-        for (int i = 0; i <= num; i++) primes[i] = true;
-
-        for (int i = 2; i <= Math.sqrt(num); i++){
-            // verifica se 'i' é primo
-            if (primes[i]){
-                // múltiplos de 'i' não são primos
-                for (int j = 2; i * j <= num; j++) {
-                    primes[i * j] = false;
-                }
-            }
-        }
-
-        List<Integer> firstPrimeNumbers = new ArrayList<>();
-        for (int i = 2; i <= num && i <= MAX_ERATOSTHENES_SIEVE_PRIMES; i++){
-            if (primes[i]) {
-                firstPrimeNumbers.add(i);
-            }
-        }
-        return firstPrimeNumbers;
-    }
+//    private List<Integer> eratosthenesSieve(BigInteger num){
+//        boolean[] primes = new boolean[num.intValue() + 1];
+//        for (int i = 0; i <= num.intValue(); i++) primes[i] = true;
+//
+//        for (int i = 2; i <= Math.sqrt(num.intValue()); i++){
+//            // verifica se 'i' é primo
+//            if (primes[i]){
+//                // múltiplos de 'i' não são primos
+//                for (int j = 2; i * j <= num.intValue(); j++) {
+//                    primes[i * j] = false;
+//                }
+//            }
+//        }
+//
+//        List<Integer> firstPrimeNumbers = new ArrayList<>();
+//        for (int i = 2; i <= num.intValue() && i <= MAX_ERATOSTHENES_SIEVE_PRIMES; i++){
+//            if (primes[i]) {
+//                firstPrimeNumbers.add(i);
+//            }
+//        }
+//        return firstPrimeNumbers;
+//    }
 
     private boolean millerRabin(int prime){
         long until = prime - 1;
@@ -68,17 +74,33 @@ public class KeyGenerator {
             long r = Math.abs(random.nextLong());
             long a = r % (prime - 1) + 1;
             long temp = until;
-            long mod = (long) (Math.pow(a, temp) % prime);
+            long mod = modPow(a, temp, prime);
 
             while (temp != prime - 1 && mod != 1 && mod != prime - 1){
-                mod = BigInteger.valueOf(mod).multiply(BigInteger.valueOf(mod))
-                        .mod(BigInteger.valueOf(prime))
-                        .longValue();
+                mod = mulMod(mod, mod, prime);
                 temp *= 2;
             }
             if (mod != prime - 1 && temp % 2 == 0) return false;
         }
         return true;
+    }
+
+    //(a ^ b) % c
+    public long modPow(long a, long b, long c){
+        long res = 1;
+        for (int i = 0; i < b; i++){
+            res *= a;
+            res %= c;
+        }
+        return res % c;
+    }
+
+    //(a * b) % c
+    public long mulMod(long a, long b, long mod){
+        return BigInteger.valueOf(a)
+                .multiply(BigInteger.valueOf(b))
+                .mod(BigInteger.valueOf(mod))
+                .longValue();
     }
 
 }
---
+++ src/main/java/RSA/tempMain.java
@@ -0,0 +1,8 @@
+package RSA;
+
+public class tempMain {
+    public static void main(String[] args) {
+        KeyGenerator keyGenerator = new KeyGenerator();
+        System.out.println(keyGenerator.getRandomPrimeNumber());
+    }
+}
